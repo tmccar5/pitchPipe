e76800823feb7092224f74210d90cec94391d498
tmccar5 <tommccarthyguitar@gmail.comssh-keygen -t rsa -C tommccarthyguitar@gmail.comcat>
2015-12-09 19:04:46 -0500

    alert window and some audio

---
+++ index.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+	
+	var words = "please work"
+
+	// $( "#target" ).click(function() {
+	//   alert( "Handler for .click() called." );
+	// });
+
+
+	$("#btn").click(function() {
+		alert(words);
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function(){
+// 	var test = document.getElementById("myBtn")
+
+// 	test.addEventListener("click", carBoy);
+
+// 		function carGo(){
+// 			go = window.alert("please work")
+// 			return
+// 		};
+
+// };
+
+
+
+
+
+
+
+// function test(){
+// 	var yes = alert("please work")
+// 	return yes
+// };
+
+// // 	{
+// document.getElementById("myBtn").addEventListener("click", test);
+
+// // var A = document.getElementByID("a");
+
+// // document.getElementById("myBtn").addEventListener("click", function(){
+// //     alert("Hello World!");
+// // });
