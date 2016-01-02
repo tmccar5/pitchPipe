1042eb2b3baca079b3dc13602eec1ae08bfdfe43
tmccar5 <tommccarthyguitar@gmail.comssh-keygen -t rsa -C tommccarthyguitar@gmail.comcat>
2016-01-02 15:09:56 -0500

    Buttons & Tones

--- index.js
+++ index.js
@@ -1,16 +1,247 @@
-$(document).ready(function(){
-	
-	var words = "please work"
+$(document).ready(function() {
+  function initAudioPlayer () {
+    //A toggle click
+    audioA = new Audio();
+    audioA.src = "A.wav";
+    audioA.loop = true;
+    playbtn = document.getElementById("A");
+    playbtn.addEventListener("click", playPauseA);
+    function playPauseA(){
+      if(audioA.paused) {
+        audioA.play();
+      }
+      else {
+        audioA.pause();
+      }
+    }
+    //Bb toggle click
+    audioBb = new Audio();
+    audioBb.src = "Bb.wav";
+    audioBb.loop = true;
+    playbtn = document.getElementById("Bb");
+    playbtn.addEventListener("click", playPauseBb);
+    function playPauseBb(){
+      if(audioBb.paused) {
+        audioBb.play();
+      }
+      else {
+        audioBb.pause();
+      }
+    }
+     //B toggle click
+    audioB = new Audio();
+    audioB.src = "B.wav";
+    audioB.loop = true;
+    playbtn = document.getElementById("B");
+    playbtn.addEventListener("click", playPauseB);
+    function playPauseB(){
+      if(audioB.paused) {
+        audioB.play();
+      }
+      else {
+        audioB.pause();
+      }
+    }
+    //C toggle click
+    audioC = new Audio();
+    audioC.src = "C.wav";
+    audioC.loop = true;
+    playbtn = document.getElementById("C");
+    playbtn.addEventListener("click", playPauseC);
+    function playPauseC(){
+      if(audioC.paused) {
+        audioC.play();
+      }
+      else {
+        audioC.pause();
+      }
+    }
+    //Db toggle click
+    audioDb = new Audio();
+    audioDb.src = "Db.wav";
+    audioDb.loop = true;
+    playbtn = document.getElementById("Db");
+    playbtn.addEventListener("click", playPauseDb);
+    function playPauseDb(){
+      if(audioDb.paused) {
+        audioDb.play();
+      }
+      else {
+        audioDb.pause();
+      }
+    }
+    //D toggle click
+    audioD = new Audio();
+    audioD.src = "D.wav";
+    audioD.loop = true;
+    playbtn = document.getElementById("D");
+    playbtn.addEventListener("click", playPauseD);
+    function playPauseD(){
+      if(audioD.paused) {
+        audioD.play();
+      }
+      else {
+        audioD.pause();
+      }
+    }
+    //Eb toggle click
+    audioEb = new Audio();
+    audioEb.src = "Eb.wav";
+    audioEb.loop = true;
+    playbtn = document.getElementById("Eb");
+    playbtn.addEventListener("click", playPauseEb);
+    function playPauseEb(){
+      if(audioEb.paused) {
+        audioEb.play();
+      }
+      else {
+        audioEb.pause();
+      }
+    }
+      //E toggle click
+    audioE = new Audio();
+    audioE.src = "E.wav";
+    audioE.loop = true;
+    playbtn = document.getElementById("E");
+    playbtn.addEventListener("click", playPauseE);
+    function playPauseE(){
+      if(audioE.paused) {
+        audioE.play();
+      }
+      else {
+        audioE.pause();
+      }
+    }
+      //F toggle click
+    audioF = new Audio();
+    audioF.src = "F.wav";
+    audioF.loop = true;
+    playbtn = document.getElementById("F");
+    playbtn.addEventListener("click", playPauseF);
+    function playPauseF(){
+      if(audioF.paused) {
+        audioF.play();
+      }
+      else {
+        audioF.pause();
+      }
+    }
+      //Gb toggle click
+    audioGb = new Audio();
+    audioGb.src = "Gb.wav";
+    audioGb.loop = true;
+    playbtn = document.getElementById("Gb");
+    playbtn.addEventListener("click", playPauseGb);
+    function playPauseGb(){
+      if(audioGb.paused) {
+        audioGb.play();
+      }
+      else {
+        audioGb.pause();
+      }
+    }
+      //G toggle click
+    audioG = new Audio();
+    audioG.src = "G.wav";
+    audioG.loop = true;
+    playbtn = document.getElementById("G");
+    playbtn.addEventListener("click", playPauseG);
+    function playPauseG(){
+      if(audioG.paused) {
+        audioG.play();
+      }
+      else {
+        audioG.pause();
+      }
+    }
+      //Ab toggle click
+    audioAb = new Audio();
+    audioAb.src = "Ab.wav";
+    audioAb.loop = true;
+    playbtn = document.getElementById("Ab");
+    playbtn.addEventListener("click", playPauseAb);
+    function playPauseAb(){
+      if(audioAb.paused) {
+        audioAb.play();
+      }
+      else {
+        audioAb.pause();
+      }
+    }
 
-	$(":button").click(function() {
-		alert(words);
-	});
 
+
+
+
+
+
+
+
+  }
+  window.addEventListener("load", initAudioPlayer);
 });
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var audio, playbtn, seek_bar;
+  // function initAudioPlayer () {
+  //   audio = new Audio();
+  //   audio.src = "snare.wav";
+  //   audio.loop = true;
+  // }
+  // window.addEventListener("load", initAudioPlayer);
+
+	// var note_tone = "#note_tone"
+
+	// $(":button").click(function() {
+	// 	play(note_tone);
+	// });
+
+
+
+
+// $(document).ready(function(){
+	
+// // 	var click = $("#btn").click(function(){
+// // 		$("#tone").Play();
+// // 	})
+
+// // });
 
 
 
